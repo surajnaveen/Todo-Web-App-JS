470712da038fe2407e27c030a40a30b12fb48c66
Suraj Naveen <suraj0nb@gmail.com>
2023-09-05 23:00:10 +0530

    Structure, design and add function

---
+++ script.js
@@ -0,0 +1,47 @@
+const addTask = document.getElementById("taskAdding");
+const addDate = document.getElementById("dateAdding");
+const subTaskAdding = document.getElementById("taskbar");
+
+const adding = document.getElementById("addBtn");
+
+const tasksList=[]
+
+/* Show items when loading the page */
+loadingPage();
+
+function loadingPage(){
+const sethtml = tasksList.map(function(Items){
+    var result = `<div class="tasks">
+    <div class="title-div">
+      <h3 class="subtitle date">${Items.title}</h3>
+    </div>
+    <div class="date-div">
+      <h3 class="subtitle date">${Items.date}</h3>
+    </div>
+    <i class="fa-regular fa-trash-can"></i>
+    <i class="fa-regular fa-pen-to-square"></i>
+  </div>`;
+
+  return result;
+});
+
+subTaskAdding.innerHTML=sethtml.join('');
+}
+
+function inputInto(){
+    var newdate = new Date(addDate.value);
+    var pendingTasks=
+        {
+            title: addTask.value,
+            date: newdate.toDateString()
+        }
+
+    tasksList.push(pendingTasks);
+    loadingPage();
+
+}
+
+adding.addEventListener("click",function(){
+    event.preventDefault();
+    inputInto();
+})
